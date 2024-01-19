e77b4e859f78b53179207bc761fea3b9c4563f72
Vishal Singh Yadav <vishal637yadav@gmail.com>
2024-01-19 14:47:17 +0530

    Factory Design Pattern Example

---
+++ src/code/structural/dp/factory/CommercialPlan.java
@@ -0,0 +1,9 @@
+package code.structural.dp.factory;
+
+public class CommercialPlan extends Plan {
+
+    public void getRate(){
+        rate=7.50;
+    }
+
+}
---
+++ src/code/structural/dp/factory/InstitutionalPlan.java
@@ -0,0 +1,9 @@
+package code.structural.dp.factory;
+
+public class InstitutionalPlan extends Plan {
+
+    public void getRate(){
+        rate=5.50;
+    }
+
+}
